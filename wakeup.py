e308274ba76b454544dd9747f8fae9bd0a08f655
Salman Faris <salmanfaris98@gmail.com>
2020-10-13 09:02:31 +0100

    Revert "Cleaned up code for wakeup"

--- wakeup.py
+++ wakeup.py
@@ -1,9 +1,6 @@
 from termcolor import colored
 
 def main():
-    # The additional hours body needed to shut down
-    body_sleep_hours_required = 1
-    
     while True:
         try:
             sleep_time = int(input("What time are you sleeping: "))
@@ -29,13 +26,15 @@ def main():
                 error_msg()
             except ValueError:
                 error_msg()
-        
+    
+    # The additional hours body needed to shut down
+    body_sleep_hours_required = 1
+    
     adjusted_hours = sleep_hours_wanted + body_sleep_hours_required
     wakeup_time = (sleep_time + adjusted_hours) % 12
     
     print(colored(
         f"Set your alarm at: {wakeup_time} AM. Have a good sleep!", 'green'))
-
 
 def error_msg():
     err_msg = "Error: Time must be an int between 1 and 12."
